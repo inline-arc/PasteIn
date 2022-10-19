799fe4a90d9f88a6dae6c67b727f952e6a680dbf
inline-arc <atharvmore20@gmail.com>
2022-10-19 15:45:26 -0400

    db models

--- pastein/form/admin.py
+++ pastein/form/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
 # Register your models here.
+
+from .models import form
+
+
+#register site
+admin.site.register(form)
--- pastein/form/models.py
+++ pastein/form/models.py
@@ -1,3 +1,14 @@
+from configparser import MAX_INTERPOLATION_DEPTH
+from pyexpat import model
+from turtle import title
 from django.db import models
 
 # Create your models here.
+
+class form(models.Model):
+    title = models.CharField('title',max_length=100)
+    link = models.URLField('links')
+    textarea = models.TextField(blank=False)
+
+    def __str__(self):
+        return self.title
